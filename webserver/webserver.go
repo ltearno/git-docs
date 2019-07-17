0e5ba4cf45c070109a3d51c9ec17b0f7e246344a
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-17 21:07:32 +0200

    wip

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -2,16 +2,38 @@ package webserver
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
+	"../assetsgen"
 	"../repository"
 )
 
+type PageContext struct {
+	Name string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	context := &PageContext{
+		Name: "First context member",
+	}
+
 	name := r.URL.Path[len("/webui/"):]
 
-	fmt.Fprintf(w, "Hi there, I love %s!", name)
+	rawTemplateBytes, err := assetsgen.Asset("assets/" + name)
+	if err != nil {
+		fmt.Fprintf(w, "Sorry, nothing here (%s)", name)
+	} else {
+		rawTemplate := string(rawTemplateBytes)
+		t, err := template.New(name).Parse(string(rawTemplate))
+		if err != nil {
+			fmt.Fprintf(w, "Sorry, internal problem")
+			fmt.Printf("error cannot parse %s %v", name, err)
+		} else {
+			t.Execute(w, context)
+		}
+	}
 }
 
 /* Run runs the Web server... */
