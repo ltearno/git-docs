5a54d7701f6a4ddcf1c82ba51894dacfc44e9008
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-17 21:17:08 +0200

    wip

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -14,33 +14,40 @@ type PageContext struct {
 	Name string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request, path string) {
 	context := &PageContext{
 		Name: "First context member",
 	}
 
-	name := r.URL.Path[len("/webui/"):]
-
-	rawTemplateBytes, err := assetsgen.Asset("assets/" + name)
+	rawTemplateBytes, err := assetsgen.Asset("assets/" + path)
 	if err != nil {
-		fmt.Fprintf(w, "Sorry, nothing here (%s)", name)
+		fmt.Fprintf(w, "Sorry, nothing here (%s)", path)
 	} else {
 		rawTemplate := string(rawTemplateBytes)
-		t, err := template.New(name).Parse(string(rawTemplate))
+		t, err := template.New(path).Parse(string(rawTemplate))
 		if err != nil {
 			fmt.Fprintf(w, "Sorry, internal problem")
-			fmt.Printf("error cannot parse %s %v", name, err)
+			fmt.Printf("error cannot parse %s %v", path, err)
 		} else {
 			t.Execute(w, context)
 		}
 	}
 }
 
+func addHandler(path string, fn func(http.ResponseWriter, *http.Request, string)) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path[len(path):]
+		fn(w, r, path)
+	}
+
+	http.HandleFunc(path, handler)
+}
+
 /* Run runs the Web server... */
 func Run(magic *repository.MagicGitRepository) {
 	fmt.Println("starting web server")
 
-	http.HandleFunc("/webui/", handler)
+	addHandler("/webui/", handler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
